5490e4baa0e0c51e4e2fd1d66e49f943bf514891
Cole Maxwell <cole.n.maxwell99@gmail.com>
2022-02-17 16:10:42 -0600

    Move todos service tests
    Co-Authored-By: Yubo Mao <mao00071@morris.umn.edu>

--- client/src/app/todos/todos.service.spec.ts
+++ client/src/app/todos/todos.service.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
-import { TodosService } from './todos/todos.service';
+import { TodosService } from './todos.service';
+import { ToDo } from './todo';
 
 describe('TodosService', () => {
   let service: TodosService;
