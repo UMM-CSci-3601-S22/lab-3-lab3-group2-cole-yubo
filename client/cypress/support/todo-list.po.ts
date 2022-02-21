7e2e7b2ad8b3dc5d7f7d188624aa454dc2f87095
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2022-02-21 12:51:47 -0600

    Start e2e tests for todos page

---
+++ client/cypress/support/todo-list.po.ts
@@ -0,0 +1,40 @@
+export class TodoListPage {
+  navigateTo() {
+    return cy.visit('/todos');
+  }
+
+  getUrl() {
+    return cy.url();
+  }
+
+  /**
+   * Gets the title of the app when visiting the `/todos` page.
+   *
+   * @returns the value of the element with the ID `.todos-list-title`
+   */
+  getTodoTitle() {
+    return cy.get('.todos-list-title');
+  }
+
+  /**
+   * Get all the `.todo-nav-list` DOM elements. This will
+   * be empty if we're using the card view of the todos.
+   *
+   * @returns an iterable (`Cypress.Chainable`) containing all
+   *   the `.todo-list-item` DOM elements.
+   */
+  getTodoListItems() {
+    return cy.get('.todo-nav-list .todo-list-item');
+  }
+
+  /**
+   * Clicks the "view profile" button for the given todo card.
+   * Requires being in the "card" view.
+   *
+   * @param listItem The todo item
+   */
+  clickTodoItem(listItem: Cypress.Chainable<JQuery<HTMLElement>>) {
+    return listItem.find<HTMLButtonElement>('[data-test="todoListItem"]').click();
+  }
+
+}
